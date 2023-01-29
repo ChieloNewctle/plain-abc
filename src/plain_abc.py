4110bfd43c32c709ab096cb2390dfec2d858a9f8
Chielo Newctle <ChieloNewctle@Yandex.com>
2023-01-29 15:10:43 +0800

    update plain_abc.py

--- src/plain_abc.py
+++ src/plain_abc.py
@@ -2,11 +2,10 @@
 An ABC implementation without metaclass
 better compatibility with other libraries
 """
+import inspect
 import sys
 from abc import abstractmethod
-from inspect import signature
 from itertools import dropwhile
-from typing import Callable, Iterable, Tuple
 
 if sys.version_info < (3, 8):
     from importlib_metadata import version
@@ -16,7 +15,7 @@ else:
 __version__ = version('plain-abc')
 
 
-def is_abstractmethod(f: Callable) -> bool:
+def is_abstractmember(f) -> bool:
     return getattr(f, '__isabstractmethod__', False)
 
 
@@ -25,12 +24,12 @@ def __sanity_check():
     def f():
         ...
 
-    assert is_abstractmethod(f)
+    assert is_abstractmember(f)
 
     def g():
         ...
 
-    assert not is_abstractmethod(g)
+    assert not is_abstractmember(g)
 
 
 __sanity_check()
@@ -41,59 +40,78 @@ class PlainABCError(Exception):
 
 
 class NameConflictError(PlainABCError):
-    def __init__(self, name, method, defined, *args, **kwargs):
-        self.name, self.method, self.defined = name, method, defined
-        message = f'method {name} should be defined only once'
+    def __init__(self, current, pre_defined, *args, **kwargs):
+        self.current, self.pre_defined = current, pre_defined
+        tp, name, _ = current
+        message = f'name conflict in {tp}, abstract member {name} should be defined only once'
         super().__init__(message, *args, **kwargs)
 
 
 class WrongImplError(PlainABCError):
-    def __init__(self, name, method, defined, *args, **kwargs):
-        self.name, self.method, self.defined = name, method, defined
-        message = f'wrong implementation for method {name}'
+    def __init__(self, current, pre_defined, *args, **kwargs):
+        self.current, self.pre_defined = current, pre_defined
+        tp, name, _ = current
+        message = f'wrong implementation for member {name} in {tp}'
         super().__init__(message, *args, **kwargs)
 
 
 class MissingImplError(PlainABCError):
-    def __init__(self, missing_methods, *args, **kwargs):
-        self.missing_methods = tuple(missing_methods)
-        method_signatures = ', '.join(
-            map(str, map(signature, self.missing_methods[:3]))
-        )
-        missing_more = '...' if len(missing_methods) > 3 else ''
-        message = (
-            f'missing implementation for {method_signatures}{missing_more}'
-        )
+    def __init__(self, cls, missing, *args, **kwargs):
+        self.cls, self.missing = cls, tuple(sorted(missing))
+        missing_names = ', '.join(self.missing[:3])
+        missing_more = '...' if len(self.missing) > 3 else ''
+        message = f'missing implementation in {cls} for {missing_names}{missing_more}'
         super().__init__(message, *args, **kwargs)
 
 
 class PlainABC(object):
     @classmethod
-    def _get_abstract_methods(cls) -> Iterable[Tuple[str, Callable]]:
+    def _plain_abc_members_to_verify(cls):
         return (
-            (name, attr)
+            (tp, name, attr)
             for tp in dropwhile(lambda x: x != PlainABC, reversed(cls.__mro__))
+            if issubclass(tp, PlainABC)
             for name, attr in tp.__dict__.items()
-            if not name.startswith('__') and callable(attr)
+        )
+
+    @classmethod
+    def _plain_abc_member_signature(cls, name, member):
+        if isinstance(member, property):
+            return property
+        if isinstance(member, (classmethod, staticmethod)):
+            return inspect.signature(getattr(cls, name))
+        if callable(member):
+            return inspect.signature(member)
+        raise TypeError(
+            f'unknown type of member {name}: {member} to get the signature'
         )
 
     @classmethod
     def __init_subclass__(cls, **kwargs):
         defined, covered = dict(), set()
 
-        for name, method in cls._get_abstract_methods():
-            if is_abstractmethod(method):
+        for tp, name, attr in cls._plain_abc_members_to_verify():
+            if is_abstractmember(attr):
                 if name in defined:
-                    raise NameConflictError(name, method, defined[name])
-                defined[name] = method
+                    raise NameConflictError((tp, name, attr), defined[name])
+
+                defined[name] = tp, name, attr
             elif name in defined:
-                if signature(method) != signature(defined[name]):
-                    raise WrongImplError(name, method, defined[name])
+                defined_tp, _, defined_attr = defined[name]
+                defined_signature = defined_tp._plain_abc_member_signature(
+                    name, defined_attr
+                )
+
+                current_signature = tp._plain_abc_member_signature(name, attr)
+
+                if current_signature != defined_signature:
+                    raise WrongImplError((tp, name, attr), defined[name])
+
                 covered.add(name)
 
         if PlainABC not in cls.__bases__:
-            uncovered = frozenset(defined.keys()).difference(covered)
-            if uncovered:
-                raise MissingImplError(tuple(map(defined.get, uncovered)))
+            missing = frozenset(defined.keys()).difference(covered)
+            if missing:
+                raise MissingImplError(cls, missing)
 
         return super().__init_subclass__(**kwargs)
