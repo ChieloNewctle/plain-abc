3b9ef113e02a9f9116a2b5c33d93fdb629642739
Dmitrii <tokarenko@outlook.com>
2023-11-26 13:41:18 +0300

    Fix TypeError: unknown type of member
    - Fix https://github.com/ChieloNewctle/plain-abc/issues/1

--- tests/test_plain_abc.py
+++ tests/test_plain_abc.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 import pytest
+from enum import Enum
+from typing import Literal
 
 from plain_abc import (MissingImplError, NameConflictError, PlainABC,
                        WrongImplError)
@@ -188,3 +190,19 @@ def test_plain_abc_conflict():
 
         class _(ARecord, IFooA, IFooB):
             ...
+
+def test_plain_abc_enum():
+    class IWordSizeEnum(PlainABC):
+        @property
+        @abstractmethod
+        def x32(self) -> Literal["x32"]:
+            ...
+
+        @property
+        @abstractmethod
+        def x64(self) -> Literal["x64"]:
+            ...
+
+    class WordSizeEnum(Enum):
+        x32 = "x32"
+        x64 = "x64"
