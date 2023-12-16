82eb65cad854ebfb4dcb2ff62e8f69b339190e8d
Chielo Newctle <ChieloNewctle@gmail.com>
2023-12-16 18:53:47 +0800

    update for newer python

--- plain_abc.py
+++ plain_abc.py
@@ -3,14 +3,9 @@ An ABC implementation without metaclass
 better compatibility with other libraries
 """
 import inspect
-import sys
 from abc import abstractmethod
+from importlib.metadata import version
 from itertools import dropwhile
-
-if sys.version_info < (3, 8):
-    from importlib_metadata import version
-else:
-    from importlib.metadata import version
 
 __version__ = version("plain-abc")
 
@@ -40,31 +35,41 @@ class PlainABCError(Exception):
 
 
 class NameConflictError(PlainABCError):
-    def __init__(self, current, pre_defined, *args, **kwargs):
-        self.current, self.pre_defined = current, pre_defined
+    @classmethod
+    def from_context(cls, current, pre_defined, **kwargs):
         tp, name, _ = current
         message = (
             f"name conflict in {tp}, "
             f"abstract member {name} should be defined only once"
         )
-        super().__init__(message, *args, **kwargs)
+        res = cls(message, **kwargs)
+        if hasattr(res, "add_note"):
+            res.add_note(f"{pre_defined=}")
+        return res
 
 
 class WrongImplError(PlainABCError):
-    def __init__(self, current, pre_defined, *args, **kwargs):
-        self.current, self.pre_defined = current, pre_defined
+    @classmethod
+    def from_context(cls, current, pre_defined, **kwargs):
         tp, name, _ = current
         message = f"wrong implementation for member {name} in {tp}"
-        super().__init__(message, *args, **kwargs)
+        res = cls(message, **kwargs)
+        if hasattr(res, "add_note"):
+            res.add_note(f"{pre_defined=}")
+        return res
 
 
 class MissingImplError(PlainABCError):
-    def __init__(self, cls, missing, *args, **kwargs):
-        self.cls, self.missing = cls, tuple(sorted(missing))
-        missing_names = ", ".join(self.missing[:3])
-        missing_more = "..." if len(self.missing) > 3 else ""
-        message = f"missing implementation in {cls} for {missing_names}{missing_more}"
-        super().__init__(message, *args, **kwargs)
+    @classmethod
+    def from_context(cls, clazz, missing, **kwargs):
+        missing = tuple(sorted(missing))
+        missing_names = ", ".join(missing[:3])
+        missing_more = "..." if len(missing) > 3 else ""
+        message = f"missing implementation in {clazz} for {missing_names}{missing_more}"
+        res = cls(message, **kwargs)
+        if missing_more and hasattr(res, "add_note"):
+            res.add_note(f"{missing=}")
+        return res
 
 
 class PlainABC(object):
@@ -111,7 +116,9 @@ class PlainABC(object):
 
             if is_abstractmember(attr):
                 if name in defined:
-                    raise NameConflictError((tp, name, attr), defined[name])
+                    raise NameConflictError.from_context(
+                        (tp, name, attr), defined[name]
+                    )
 
                 defined[name] = tp, name, attr
             elif name in defined:
@@ -123,13 +130,13 @@ class PlainABC(object):
                 current_signature = tp._plain_abc_member_signature(name, attr)
 
                 if current_signature != defined_signature:
-                    raise WrongImplError((tp, name, attr), defined[name])
+                    raise WrongImplError.from_context((tp, name, attr), defined[name])
 
                 covered.add(name)
 
         if PlainABC not in cls.__bases__:
             missing = frozenset(defined.keys()).difference(covered)
             if missing:
-                raise MissingImplError(cls, missing)
+                raise MissingImplError.from_context(cls, missing)
 
         return super().__init_subclass__(**kwargs)
